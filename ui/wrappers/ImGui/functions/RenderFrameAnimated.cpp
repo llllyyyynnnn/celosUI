9c6e5f264d6561b17f866b180d5e6833ce664204
eve <eve@eve.eve>
2024-01-31 14:30:36 +0100

    x

--- ui/wrappers/ImGui/functions/RenderFrameAnimated.cpp
+++ ui/wrappers/ImGui/functions/RenderFrameAnimated.cpp
@@ -6,8 +6,7 @@ void ImGui::RenderFrameAnimated(const char* label, ImVec2 p_min, ImVec2 p_max, I
     ImGuiWindow* window = g.CurrentWindow;
 
     std::string std_label = (std::string)label;
-    std::string name = "RenderFrameAnimated";
-
-    ImColor animated_color = celosia::animations::set(std_label+name, ImVec4(fill_col), 0.f, celosia::animations::e_method::linear);
+    
+    ImColor animated_color = celosia::animations::set(std_label + "RenderFrameAnimated", ImVec4(fill_col), 0.f, celosia::animations::e_method::linear);
     window->DrawList->AddRectFilled(p_min, p_max, animated_color, rounding);
 }
